50e9abe9ea7739ced41ee87a2b456b2f8da19774
Ryan Behdad <Ryan.Behdad@cashconverters.com>
2020-06-27 09:04:59 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pylatex as pl
+import pandas as pd
+import subprocess
+
+file_name = 'report'
+csv_file = 'data.csv'
+threshold = 2_000_000_000
+latex_path = 'C:/Users/ryan.behdad/AppData/Local/Programs/MiKTeX 2.9/miktex/bin/x64/'
+
+df = pd.read_csv(csv_file)
+df['Double CF'] = df['CF'] * 2
+if df['Double CF'].sum() > threshold:
+    sentence = 'The profit of this project is ' + str(df['Double CF'].sum())
+else:
+    sentence = 'The loss of this project is ' + str(-1 * df['Double CF'].sum())
+
+doc = pl.Document()
+doc.packages.append(pl.Package('booktabs'))
+
+with doc.create(pl.Section('Results')):
+    with doc.create(pl.Table(position='htbp')) as table:
+        table.add_caption('Test')
+        table.append(pl.Command('centering'))
+        table.append(pl.NoEscape(df.to_latex(escape=False, index=False)))
+    
+    doc.append(sentence)
+
+
+def create_pdf(filename):
+    input_filename = file_name + '.tex'
+    output_filename = file_name
+    process = subprocess.Popen([
+        latex_path + 'latex',
+        '-output-format=pdf',
+        '-job-name=' + output_filename,
+        input_filename])
+    process.wait()
+
+doc.generate_tex(file_name)
+create_pdf(file_name)
